44f2409e2c2203f6608c0e84fa2dc6f17d325e2b
amitgupta199929 <amitgupta199929@gmail.com>
2026-09-16 21:25:17 +0530

    Add files via upload

---
+++ Restore Point Collection Deletion.ps1
@@ -0,0 +1,22 @@
+﻿Connect-AzAccount
+$data = Import-csv -Path ""
+foreach ($datsource in $data){
+$ResourceGroupName = "<RESOURCE_GROUP_NAME>"
+$resourceName = ""
+$subscription = ""
+$sub = Select-AzSubscription -Name $subscription
+Get Restore Point Collections
+$RPCs = Get-AzResource -Name $resourceName -ResourceGroupName $ResourceGroupName -ResourceType "Microsoft.Compute/restorePointCollections"
+
+if (-not $RPCs) { Write-Host "No Restore Point Collections found." -ForegroundColor Yellow return }
+
+Write-Output "`nRestore Point Collections found:" -ForegroundColor Cyan Write-Output "-----------------------------------"
+
+Print RPC details
+$RPCs | Select-Object Name, ResourceGroupName, Location, ResourceId | Format-Table -AutoSize
+
+Remove-AzResource -ResourceId $RPC.ResourceId -Force -ErrorAction Continue
+
+Write-Output "Successfully deleted: $($RPC.Name)" -ForegroundColor Green
+
+}
